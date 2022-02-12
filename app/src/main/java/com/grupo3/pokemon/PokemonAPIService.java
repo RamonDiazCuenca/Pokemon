8e3fd439d65557d210a7a9970a070709e858139c
RamonDiazCuenca <ramon.dc92@gmail.com>
2022-02-12 13:07:29 +0100

    0.2

---
+++ app/src/main/java/com/grupo3/pokemon/PokemonAPIService.java
@@ -0,0 +1,10 @@
+package com.grupo3.pokemon;
+
+import retrofit2.http.GET;
+import retrofit2.Call;
+
+public interface PokemonAPIService {
+
+    @GET("pokemon/?limit=50")
+    Call getPokemons();
+}
---
+++ app/src/main/java/com/grupo3/pokemon/PokemonFetchResults.java
@@ -0,0 +1,17 @@
+package com.grupo3.pokemon;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class PokemonFetchResults {
+
+    @SerializedName("results")
+    @Expose
+    private ArrayList results;
+
+    public ArrayList getResults() {
+        return results;
+    }
+}
